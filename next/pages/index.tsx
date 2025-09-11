9db181e7387c20c5d089e796657dca296d07d60f
MichelKukresh <kukreshmihal@yandex.ru>
2025-09-11 09:36:48 +0300

    подготовка проекта

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,4 +1,5 @@
 import { protectedPage } from "@/features/auth";
 import { HomePage } from "@/pages/home";
 
-export default protectedPage(HomePage);
+//export default protectedPage(HomePage); // TO-DO убрать обертку, убрать класс protectedPage
+export default HomePage;
