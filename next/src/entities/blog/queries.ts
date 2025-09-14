e071f0aa9b08e391914489963eb65b2437683aff
MichelKukresh <kukreshmihal@yandex.ru>
2025-09-14 18:06:33 +0300

    начал api

---
+++ next/src/entities/blog/queries.ts
@@ -0,0 +1,13 @@
+// import { useQuery } from "@tanstack/react-query";
+
+// const blockListKey = ["blog"] as unknown[];
+
+// export function useBlockListQuery({ q }: { q?: string }) {
+//   return useQuery({
+//     queryKey: blockListKey.concat([{ q }]),
+//     queryFn: () => blockListControllerGetList({ q }),
+//     keepPreviousData: true,
+//   });
+// }
+
+// TO-DO переработать
